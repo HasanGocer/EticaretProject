58e1e80cf17338cc87719b10d7a6d834ea239c38
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2025-02-18 13:49:49 +0300

    HG

--- react-nodejs-project/backend/routes/trademark.route.js
+++ react-nodejs-project/backend/routes/trademark.route.js
@@ -30,7 +30,7 @@ const upload = multer({ storage });
 router.post("/addHG", upload.single("image_data"), async (req, res) => {
   try {
     const { name } = req.body;
-    return res.status(400).json({ message: "Resim dosyası gereklidir." });
+    return res.status(400).json({ message: "Girdik" });
     if (!req.file) {
       return res.status(400).json({ message: "Resim dosyası gereklidir." });
     }
